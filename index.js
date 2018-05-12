b40120a02f41271f7b9cbb3a96b75d46ba390ead
savyounts@gmail.com <savyounts@gmail.com>
2018-05-12 14:42:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ function sayHiToGrandma(string)
   if (string === string.toLowerCase()) {
     return 'I can/t hear you!'; 
   }
-}
   else if (string === string.toUpperCase()) {
     return 'YES INDEED!';
   }
