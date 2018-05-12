5f28aaef3d4f4212453211099330c087f82e3971
savyounts@gmail.com <savyounts@gmail.com>
2018-05-12 14:18:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function sayHiToGrandma(string)
   else if (string === string.toUpperCase()) {
     return YES INDEED!;
   }
-  else (I love you, Grandma.) {
+  else (string === "I love you, Grandma.") {
     return I love you, too.;
   }
 }
