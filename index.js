a9b0d5ec4ef5f74a9eb6beac7e35e7ac6b372ceb
savyounts@gmail.com <savyounts@gmail.com>
2018-05-12 14:51:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,15 @@ function logWhisper(string)
   console.log(string.toLowerCase())
 }
 
+function sayHiToGrandma(string)
+{
+  if (string === string.toLowerCase()) {
+    return 'I can\'t hear you!' 
+  }
+  else if (string === string.toUpperCase()) {
+    return 'YES INDEED!'
+  }
+  else (string === I love you, Grandma.) {
+    return 'I love you, too.'
+  }
+}
